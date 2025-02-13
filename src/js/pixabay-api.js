08e9517c1f5fa543a53ffef5be89ea5fd77e984d
Bohdan Predchuk <predchukbohdan1995@ukr.net>
2025-02-13 22:53:26 +0200

    updated request script

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 export default function axiosReguest(value) {
-  const pixabayImages = axios.create({
-    baseURL: 'https://pixabay.com/api',
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '48820344-d1ffc058fda0b82549bb04a16';
+  return axios.get(BASE_URL, {
     params: {
-      key: '48820344-d1ffc058fda0b82549bb04a16',
+      key: API_KEY,
       q: value,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
     },
   });
-  return pixabayImages.get();
 }
