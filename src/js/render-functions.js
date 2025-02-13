9c719ec03498f0b208247bfaf9fe9f94ed3ffa36
Bohdan Predchuk <predchukbohdan1995@ukr.net>
2025-02-13 23:11:15 +0200

    updated request script

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -18,7 +18,7 @@ export function renderErrorMessage() {
     backgroundColor: '#EF4040',
     messageColor: 'white',
     iconColor: 'white',
-    iconUrl: '/img/Group.svg',
+    iconUrl: '../img/group.png',
     progressBar: true,
     timeout: 10000,
     animateInside: false,
